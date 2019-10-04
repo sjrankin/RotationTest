0df106e977f1329605b378cd995dffba44fc08b2
Stuart Rankin <sjrankin@gmail.com>
2019-10-04 10:27:48 +0900

    Initial Commit

---
+++ RotationTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RotationTest
+//
+//  Created by Stuart Rankin on 10/4/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
